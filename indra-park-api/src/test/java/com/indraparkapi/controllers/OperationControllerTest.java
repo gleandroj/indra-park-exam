16885a7e41f83e8221ccf98b092c58b7b1f3ce7d
Gabriel L <gabrielleandrojunior@live.com>
2019-07-04 00:23:52 -0300

    Add Controller Test

---
+++ indra-park-api/src/test/java/com/indraparkapi/controllers/OperationControllerTest.java
@@ -0,0 +1,71 @@
+package com.indraparkapi.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.indraparkapi.BaseTest;
+import com.indraparkapi.models.Operation;
+import com.indraparkapi.models.Vehicle;
+import com.indraparkapi.repositories.OperationRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@AutoConfigureMockMvc()
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OperationControllerTest extends BaseTest {
+    private static final String BASE_URL = "/api/operations";
+
+    @Autowired
+    private OperationRepository operationRepository;
+
+    @Autowired
+    private OperationController operationController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Operation operation;
+    private Vehicle car;
+    private Vehicle motorCycle;
+    private Vehicle truck;
+    private Vehicle pickup;
+
+    @Before
+    public void setUp() {
+        car = new Vehicle(Vehicle.VehicleType.Car, "Ferrari 488 Pista", "JAV0000");
+        motorCycle = new Vehicle(Vehicle.VehicleType.Motorcycle, "Ducati 1199 Panigale S", "JAV0001");
+        truck = new Vehicle(Vehicle.VehicleType.Truck, "Axor 4144 6x4", "JAV0003");
+        pickup = new Vehicle(Vehicle.VehicleType.Pickup, "Range Rover Sport SVR Facelift", "JAV0004");
+
+        operation = new Operation(car, Operation.OperationType.IN, this.now(), null);
+    }
+
+    @Test
+    public void it_should_load_context() throws Exception {
+        assertThat(operationController).isNotNull();
+    }
+
+    @Test
+    public void it_should_list_operations_without_parameters() throws Exception {
+        operationRepository.save(operation);
+
+        mockMvc.perform(get(BASE_URL)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$[0].id", is(operation.getId().intValue())));
+    }
+
+}
