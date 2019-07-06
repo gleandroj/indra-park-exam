29610a346163c4d73186991d77c3abce7445686c
Gabriel L <gabrielleandrojunior@live.com>
2019-07-05 21:01:37 -0300

    Add Vehicle Type

--- indra-park-ui/src/app/park/components/operation-list/operation-list.component.ts
+++ indra-park-ui/src/app/park/components/operation-list/operation-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Pageable } from '../../../support/interfaces/pageable';
-import { Operation } from '../../../core/entities';
+import { Operation, VehicleType, vehicleTypeDescription } from '../../../core/entities';
 
 @Component({
   selector: 'app-operation-list',
@@ -8,10 +8,14 @@ import { Operation } from '../../../core/entities';
   styleUrls: ['operation-list.component.less']
 })
 export class OperationListComponent {
-  displayedColumns = ['vehicle.plate', 'vehicle.model', 'enteredAt', 'exitedAt', 'actions'];
+  displayedColumns = ['vehicle.type', 'vehicle.plate', 'vehicle.model', 'enteredAt', 'exitedAt', 'actions'];
   pageSizeOptions = [5, 10, 25, 100];
   @Input() dataSource: Pageable<Operation> = { content: [] };
   @Output() exit = new EventEmitter();
   @Output() page = new EventEmitter();
   @Output() sort = new EventEmitter();
+
+  vehicleTypeDescription(type: VehicleType) {
+    return vehicleTypeDescription[type] || '';
+  }
 }
